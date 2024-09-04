ef4e17fd675792d925ecc4bb42d15200384a13f1
NottaZach <s11873023@students.duvalschools.org>
2024-09-04 13:20:38 -0400

    dhhgfdd

--- 00_computer_science_exercises/01_dataTypes_Operators/user_input.py
+++ 00_computer_science_exercises/01_dataTypes_Operators/user_input.py
@@ -26,10 +26,15 @@ print(float(newNumber)) # can convert STRING to FLOAT.
 print(str(newNumber)) # can convert FLOAT to STRING.
 
 # float() will convert the data to an FLOAT if possible
+newNumber = input("Please type a value and press enter.\n")
+# print (int(newNumber))<-- cannot convert FLOAT to INTEGER.
+print(float(newNumber)) # can convert STRING to FLOAT.
+print(str(newNumber)) # can convert FLOAT to STRING.
+
 # str() will convert the data to an STRING if possible
-
-
-
+newNumber = 5
+print( newNumber + newNumber) # Should print 10.
+print(str(newNumber+ newNumber)) # Should print 55.
 
 
 
