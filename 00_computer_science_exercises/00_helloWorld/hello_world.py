4fee50198c646127c2e30ad75470dfeb29c4bdf0
NottaZach <s11873023@students.duvalschools.org>
2024-09-04 13:04:55 -0400

    restore

---
+++ 00_computer_science_exercises/00_helloWorld/hello_world.py
@@ -0,0 +1,19 @@
+# Hello World, Zachariah Thomas, V0.3
+
+print() # print() is a built-in FUNCTION for Python.
+
+#SYNTAX ERRORS -- errors with the way code is written,  such as missing characters or incorrect case
+print("Hello World")
+print("New Line Character\n") # \n adds ana empty line after printing.
+
+print("How many\n lines will\n this print on\n the screen?\n")
+print("\tThis is a tab.")
+
+# Escaping the Escape Characters \
+print("\\")
+
+# Printing Single Quotes '
+print("\'")
+
+# Printing Double Quotes "
+print("\"")
---
+++ 00_computer_science_exercises/01_dataTypes_Operators/user_input.py
@@ -0,0 +1,40 @@
+# User Input Practice, Zachariah Thomas, v0.0
+
+# input() is the built-inb function to get information from the KEYBOARD
+# BASIC EXAMPLE
+variableName = input("Please type a variable name press enter.\n")
+print(variableName)
+
+# input() saves ALL INPUT TO STRING DATA TYPES BY DEFAULT!!!
+# input() saves ALL INPUT TO STRING DATA TYPES BY DEFAULT!!!
+# input() saves ALL INPUT TO STRING DATA TYPES BY DEFAULT!!!
+# input() saves ALL INPUT TO STRING DATA TYPES BY DEFAULT!!!
+
+myNumber= input(" Please type an INTEGER number and press enter.\n")
+# print(myNumber + 5) 
+
+
+
+myNumber= int(input(" Please type an INTEGER number and press enter.\n"))
+print(myNumber + 5)
+
+#Wrapper Functions
+# int() will convert the data to an INTEGER if possible
+newNumber = input("Please type a value and press enter.\n")
+print(int(newNumber)) # can convert STRING to INTEGER.
+print(float(newNumber)) # can convert STRING to FLOAT.
+print(str(newNumber)) # can convert FLOAT to STRING.
+
+# float() will convert the data to an FLOAT if possible
+# str() will convert the data to an STRING if possible
+
+
+
+
+
+
+
+
+
+
+
