3e503dc2152bedbc347bb2451d21053f67dc903a
NottaZach <s11873023@students.duvalschools.org>
2024-09-06 12:05:16 -0400

    numbers

--- 00_computer_science_exercises/02_flow_control_structures/looooooops.py
+++ 00_computer_science_exercises/02_flow_control_structures/looooooops.py
@@ -23,13 +23,13 @@ for eachFruit in fruits:
     print(eachFruit)
 
 # break Keyword -- Immediately exit the loop
-fruits=["apple", "banana", "strawberry", "blueberry"]
-for eachFruit in fruits:
-    if eachFruit== "banana":
-        break
-    print(eachFruit)
+#fruits=["apple", "banana", "strawberry", "blueberry"]
+#for eachFruit in fruits:
+    #if eachFruit== "banana":
+       # break
+    #print(eachFruit)
 
-
-
-
+# for loops using range(). range (x) is EXCLUSIVE, it starts at 0 and ends at x - 1
+for i in range(10): # range is 0-9
+    print(i)
 
