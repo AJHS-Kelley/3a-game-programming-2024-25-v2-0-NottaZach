7cfe2bddc1489c07ccca569f6265fc009f5728b0
NottaZach <s11873023@students.duvalschools.org>
2024-09-10 13:20:59 -0400

    project

---
+++ 01_gaming_exercises/00_awarding_extra_lives/awardingextralives.py
@@ -0,0 +1,58 @@
+# Awarding extra lives, Zachariah Thomas, v0.0
+
+score=10000
+lives=3
+
+# Allow the user to input the score
+
+# If score is 10000 or less
+    # Lose a Life
+# If score is > 10000 but less than 100001
+    # Give you an extra life
+# If score is >100000
+    # give you 2 extra lives
+
+# Output the score and number of lives to the screen
+
+if score <=10000:
+    print("Hahaha you lost a life. \n")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
