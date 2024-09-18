c52925bf07de3849e845945760b174b736d66e87
NottaZach <s11873023@students.duvalschools.org>
2024-09-18 11:49:54 -0400

    v0.1

---
+++ 01_gaming_exercises/04_rock_paper_scissors/rps.py
@@ -0,0 +1,44 @@
+# rock, Paper, Scissors by Zachariah Thomas, v0.1
+
+# MODULE IMPORTS
+import random
+
+# DATA STRUCTURES -- PLAYER
+playerScore = 0 
+playerName = "Test Player"
+playerChoice = None
+
+# DATA STRUCTURES -- CPU
+cpuScore = 0
+cpuChoice = None
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
